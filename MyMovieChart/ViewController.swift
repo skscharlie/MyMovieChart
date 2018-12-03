11e65eb59327437b566df217da53e9e3c7576a08
김철 <charlie@galaxia.co.kr>
2018-12-03 11:06:22 +0900

    Initial Commit

---
+++ MyMovieChart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovieChart
+//
+//  Created by user on 2018. 12. 3..
+//  Copyright © 2018년 galaxia communications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
