91a2702efa73af05656bdbc54a3935f50c3b7434
김철 <charlie@galaxia.co.kr>
2018-12-13 13:45:28 +0900

    이것도

---
+++ MyMovieChart/TheaterCell.swift
@@ -0,0 +1,16 @@
+//
+//  TheaterCell.swift
+//  MyMovieChart
+//
+//  Created by user on 2018. 12. 12..
+//  Copyright © 2018년 galaxia communications. All rights reserved.
+//
+
+import UIKit
+
+class TheaterCell : UITableViewCell {
+    
+    @IBOutlet var name: UILabel!
+    @IBOutlet var addr: UILabel!
+    @IBOutlet var tel: UILabel!
+}
